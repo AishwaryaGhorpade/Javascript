f14b1f0d2371333fdc3b13abf04cd5d18d071882
AishwaryaGhorpade <aishwaryaghorpade4@gmail.com>
2024-06-25 00:41:03 +0530

    Function in JS

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 //Functions
 function greeting(){
-    console.log("Good morning!");
+    console.log("Good morning!");  //Good morning!
 }
 greeting()
 
@@ -9,23 +9,23 @@ greeting()
 function sum(num1,num2){
     console.log(num1+num2)
 }
-sum()
-sum(3,4)
-sum(3,null)
-sum(3,"a")
-sum(3,"4")
-const res=sum(3,4)
-console.log(res)  //o.p->undefined bcz function sum is not returning anything
+sum()      //NAN
+sum(3,4)   //7
+sum(3,null)  //3
+sum(3,"a")   //3a
+sum(3,"4")   //34
+const res=sum(3,4)  
+console.log(res)  //o.p->undefined bcz function sum is not returning anything   o/p->7  undefined
 
 //return keyword
 function add(num1,num2){
     return num1+num2
 }
 const ans=add(3,4)
-console.log(ans)
-console.log(add(4,6))
+console.log(ans)      //7
+console.log(add(4,6))   //10
 
-function login(username){
+function login2(username){
     if(username===undefined)  //or we can give condition this also-> if(!username){}
     {
         console.log("please enter username")
@@ -33,8 +33,8 @@ function login(username){
     }
     return `${username} logged in`
 }
-console.log(login());
-console.log(login("Aish"))
+console.log(login2());     //please enter username   undefined  
+console.log(login2("Aish"))   //Aish logged in
 
 //default parameter value
 function login(username="Yash"){
@@ -45,19 +45,19 @@ function login(username="Yash"){
     }
     return `${username} logged in`
 }
-console.log(login());
-console.log(login("Aish"))
+console.log(login());  //Yash logged in
+console.log(login("Aish"))  //Aish logged in
 
 //Rust operator
 function calculateAllNos(...num){
     return num;
 }
-console.log(calculateAllNos(100,200,300,400,500))
+console.log(calculateAllNos(100,200,300,400,500))  //[ 100, 200, 300, 400, 500 ]
 
 function calculateAllNos2(num1,num2,...num){
     return num;
 }
-console.log(calculateAllNos2(100,200,300,400,500))
+console.log(calculateAllNos2(100,200,300,400,500))  //[ 300, 400, 500 ]
 
 //passing object in function
 function PassObject(obj)
@@ -68,11 +68,11 @@ const user={
     name:"aish",
     age:23
 }
-PassObject(user)
+PassObject(user)  //name is aish and age is 23
 
 //direct passing obj while calling function
 PassObject({
-    name:"diya",
+    name:"diya",          //name is diya and age is 3
     age:3
 })
 
@@ -81,7 +81,7 @@ function secondValue(arrayname){
     return arrayname[1];
 }
 const arr=[1,2,3,4]
-console.log(secondValue(arr))
+console.log(secondValue(arr))  //2
 
 //direct array passing while calling function
-console.log(secondValue([11,22,33,44,55]))
+console.log(secondValue([11,22,33,44,55]))  //22
